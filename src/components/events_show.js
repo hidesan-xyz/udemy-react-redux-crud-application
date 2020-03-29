757f5b9d99ca60afb339636aeb1259449ed1704d
“hidesan” <hideaki@masudaPC.local>
2020-03-29 14:43:02 +0900

    finish

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
 
 import { getEvent, deleteEvent, putEvent } from '../actions'
 
@@ -22,10 +24,14 @@ class EventsShow extends Component {
     const { input, label, type, meta: { touched, error } } = field
 
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        { touched && error && <span>{error}</span> }
-      </div>
+      <TextField 
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={ touched && error }
+        {...input}
+        fullWidth={true}
+      />
     )
   }
 
@@ -44,6 +50,7 @@ class EventsShow extends Component {
   render(){
 
     const { handleSubmit, pristine, submitting, invalid } = this.props
+    const style = { margin: 12 }
 
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -52,10 +59,11 @@ class EventsShow extends Component {
           <Field label="Body" name="body" type="text" component={this.renderFiled} />
         </div>
 
+
         <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
-          <Link to="/" >Cansel</Link>
-          <Link to="/"　onClick={this.onDeleteClick} >Delete</Link>
+          <RaisedButton label="Submit" type="submit" style={style} disabled={pristine || submitting || invalid} />
+          <RaisedButton label="Cancel" style={style} containerElement={<Link to="/" >Cansel</Link>} />
+          <RaisedButton label="Delete" style={style} onClick={this.onDeleteClick} />
         </div>
       </form>
 
